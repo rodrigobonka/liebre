c5f46268848718746ae5fb2fc2ea5745c49ccac8
Rodrigo Dominguez <soyrodrigodominguez@gmail.com>
2021-11-25 19:10:21 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mercadoliebre
+# mercadoliebre
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.use(express.static('public'));
 
 
 app.listen(3000, () => {
-    console.log('Servidor funcionando');
+    console.log('Servidor funcionando en puerto 3000');
 });
 
 app.get('/', (req, res) => {
